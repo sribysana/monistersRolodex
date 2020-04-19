6f93484ea88b35ecd158815814e2b66338c31d63
Srikanth Bysana <sribysana@gmail.com>
2020-04-20 00:41:51 +0530

    Updates

---
+++ precache-manifest.5b8c9e7781cc62781efc7cbd35cb697c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8033cdbf6e594d45f3561eadd510b6",
+    "url": "/sribysana.github.io/monistersRolodex/index.html"
+  },
+  {
+    "revision": "df11ae6d06b8c81a983c",
+    "url": "/sribysana.github.io/monistersRolodex/static/css/main.2e2254d4.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/sribysana.github.io/monistersRolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sribysana.github.io/monistersRolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df11ae6d06b8c81a983c",
+    "url": "/sribysana.github.io/monistersRolodex/static/js/main.b29d9fed.chunk.js"
+  },
+  {
+    "revision": "c338283e5a70e5442d7b",
+    "url": "/sribysana.github.io/monistersRolodex/static/js/runtime-main.475a03ff.js"
+  }
+]);
